128dfa3e0ee2a043cad2eb751b49b2eecc690043
Lucas <daddymakeli@gmail.com>
2024-03-05 16:48:55 +0200

    Update graph editor to highlight the hovered point and the selected point

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -14,7 +14,7 @@ class GraphEditor {
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (evt) => {
       const mouse = new Point(evt.offsetX, evt.offsetY);
-      this.hovered = getNearestPoint(mouse, this.graph.points);
+      this.hovered = getNearestPoint(mouse, this.graph.points, 10);
       if (this.hovered) {
         this.selected = this.hovered;
         return;
@@ -23,12 +23,20 @@ class GraphEditor {
 
       this.selected = mouse;
     });
+
+    this.canvas.addEventListener("mousemove", (evt) => {
+      const mouse = new Point(evt.offsetX, evt.offsetY);
+      this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+    });
   }
 
   display() {
     this.graph.draw(this.ctx);
     if (this.selected) {
       this.selected.draw(this.ctx, { outline: true });
+    }
+    if (this.hovered) {
+      this.hovered.draw(this.ctx, { fill: true });
     }
   }
 }
