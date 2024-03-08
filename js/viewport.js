03f0b11cfef5e26eb6f837024e0090c4cda31397
Lucas <daddymakeli@gmail.com>
2024-03-08 17:55:52 +0200

    Implement panning and zooming features.

--- js/viewport.js
+++ js/viewport.js
@@ -4,7 +4,8 @@ class Viewport {
     this.ctx = canvas.getContext("2d");
 
     this.zoom = 1;
-    this.offset = new Point(0, 0);
+    this.center = new Point(canvas.width / 2, canvas.height / 2);
+    this.offset = scale(this.center, -1);
 
     this.drag = {
       start: new Point(0, 0),
@@ -17,7 +18,10 @@ class Viewport {
   }
 
   getMouse(evt) {
-    return new Point(evt.offsetX * this.zoom, evt.offsetY * this.zoom);
+    return new Point(
+      (evt.offsetX - this.center.x) * this.zoom - this.offset.x,
+      (evt.offsetY - this.center.y) * this.zoom - this.offset.y
+    );
   }
 
   getOffset(evt) {
