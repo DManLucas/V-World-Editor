bb088e93124f0642ad7fe5371917932b17ba00af
Lucas <daddymakeli@gmail.com>
2024-03-26 16:45:08 +0200

    "Add viewpoint parameter to World and Tree draw methods for panning"
    This commit adds a `viewPoint` parameter to the `draw` methods of the `World` and `Tree` classes. This change allows for panning the view by modifying the `viewPoint` when calling the `draw` method. The `Tree` class's `draw` method has also been updated to adjust the position of the tree's top segment based on the `viewPoint`.

--- js/items/tree.js
+++ js/items/tree.js
@@ -4,10 +4,11 @@ class Tree {
     this.size = size;
   }
 
-  draw(ctx) {
+  draw(ctx, viewPoint) {
+    const diff = subtract(this.center, viewPoint);
     this.center.draw(ctx, { size: this.size, color: "green" });
 
-    const top = add(this.center, { x: -40, y: -40 });
+    const top = add(this.center, scale(diff, 0.5));
     new Segment(this.center, top).draw(ctx);
   }
 }
--- js/world.js
+++ js/world.js
@@ -167,7 +167,7 @@ class World {
     return bases;
   }
 
-  draw(ctx) {
+  draw(ctx, viewPoint) {
     for (const env of this.envelopes) {
       env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
     }
@@ -179,7 +179,7 @@ class World {
     }
 
     for (const tree of this.trees) {
-      tree.draw(ctx);
+      tree.draw(ctx, viewPoint);
     }
     for (const bld of this.buildings) {
       bld.draw(ctx);
