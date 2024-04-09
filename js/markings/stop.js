e9492dd962cce8081130ea7eb9ab1423bfc95b07
Lucas <daddymakeli@gmail.com>
2024-04-09 12:07:40 +0200

    "Add stop sign text to Stop class drawing"
    This commit adds text to the Stop class's draw method to display the word "STOP" on the sign. The text is rotated and scaled to be positioned and sized appropriately. The context is also saved and restored to ensure that these transformations do not affect other parts of the canvas.

--- js/markings/stop.js
+++ js/markings/stop.js
@@ -14,5 +14,17 @@ class Stop {
 
   draw(ctx) {
     this.poly.draw(ctx);
+    ctx.save();
+    ctx.translate(this.center.x, this.center.y);
+    ctx.rotate(angle(this.directionVector) - Math.Pi / 2);
+    ctx.scale(1, 3);
+
+    ctx.beginPath();
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.font = "bold" + this.height * 0.3 + "px Arial";
+    ctx.fillText("STOP", 0, 1);
+    ctx.restore();
   }
 }
